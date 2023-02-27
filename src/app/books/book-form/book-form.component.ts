3b4087299f3ab83f1dc3998c12d11bb50da1c215
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-02-27 20:47:53 -0300

    Submissão de registro

--- src/app/books/book-form/book-form.component.ts
+++ src/app/books/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ControlConfig } from '@angular/forms';
+import { FormBuilder, FormGroup} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { BooksService } from '../services/books.service';
 
@@ -13,7 +14,8 @@ export class BookFormComponent implements OnInit{
   form: FormGroup;
   constructor(private formBuilder: FormBuilder,
     private service: BooksService,
-    private snackBar: MatSnackBar){
+    private snackBar: MatSnackBar,
+    private location: Location){
     this.form = this.formBuilder.group({
       name: [null],
       year: [null],
@@ -28,13 +30,17 @@ export class BookFormComponent implements OnInit{
 
   onSubmit(){
     this.service.save(this.form.value)
-    .subscribe(data => console.log(data), error => this.onError());
+    .subscribe(result => this.onSuccess(), error => this.onError());
   }
   onCancel(){
-
+    this.location.back();
   }
 
   private onError(){
     this.snackBar.open("Erro ao cadastrar livro", '',{duration: 3000})
+  }
+  private onSuccess(){
+    this.snackBar.open("Livro cadastrado com Sucesso", '',{duration: 3000});
+    this.onCancel();
   }
 }
