a825b7be1b18e84106abaacaeb0afc25be7f8bff
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-02-28 15:31:58 -0300

    Adicionando validações ao front
    nos campos de livros

--- src/app/books/book-form/book-form.component.ts
+++ src/app/books/book-form/book-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder, Validators} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from '../models/book';
@@ -15,10 +15,21 @@ export class BookFormComponent implements OnInit{
 
   form = this.formBuilder.group({
     id: [NaN],
-    name: [''],
-    year: [NaN],
-    author: [''],
-    gender: ['']
+    name: ['', 
+    [Validators.required,
+    Validators.minLength(5),
+    Validators.maxLength(200)
+    ]],
+    year: [NaN, 
+    [Validators.required,
+    Validators.minLength(4),
+    Validators.maxLength(4)]],
+    author: ['', 
+    [Validators.required,
+    Validators.maxLength(100)]],
+    gender: ['',
+    [Validators.required,
+      Validators.maxLength(100)]]
   });
   constructor(private formBuilder: NonNullableFormBuilder,
     private service: BooksService,
@@ -53,5 +64,21 @@ export class BookFormComponent implements OnInit{
   private onSuccess(){
     this.snackBar.open("Livro cadastrado com Sucesso", '',{duration: 3000});
     this.onCancel();
+  }
+
+  getErrorMessage(fieldName: string){
+    const field = this.form.get(fieldName)
+    if(field?.hasError('required')){
+      return 'Campo obrigatório'
+    }
+    if(field?.hasError('minlength')){
+      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres`
+    }
+    if(field?.hasError('maxlength')){
+      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 200;
+      return `Tamanho máximo deve ser de ${requiredLength} caracteres`
+    }
+    return 'Campo invalido'
   }
 }
