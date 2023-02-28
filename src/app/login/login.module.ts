66f49bb594359943b09f1f14ad126bd79f4df9ff
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-02-28 19:18:36 -0300

    Criando um componente para o login

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login/login.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    AppMaterialModule,
+    SharedModule
+  ]
+})
+export class LoginModule { }
