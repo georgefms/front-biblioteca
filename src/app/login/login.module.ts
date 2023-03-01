b8459c7c791f3d726d8b92e5af136409d3bf7743
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-02-28 23:27:11 -0300

    Tela de login provisória

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,17 +5,21 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ],
   imports: [
     CommonModule,
     LoginRoutingModule,
     AppMaterialModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BooksService } from 'src/app/books/services/books.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  constructor(booksService: BooksService,
+    private router: Router,
+    private route: ActivatedRoute){
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+  onAdd(){
+    this.router.navigateByUrl('/login/new');   
+  }
+
+  onBooks(){
+    this.router.navigateByUrl('/books');
+  }
 }
