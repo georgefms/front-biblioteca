d9eeefa4cfa9c3df1db925beac66f69fb575f0e2
George Felipe de Magalhães Silva <georgefelipe8@gmail.com>
2023-02-28 23:26:19 -0300

    Editando as rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    {
-      path: '',
-      pathMatch: 'full',
+  {path: '', pathMatch: 'full',redirectTo: 'login'},  
+  {
+      path: 'login',
       loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
     },
     {
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  { path: '', component: LoginComponent},
+  { path: 'new', component: LoginFormComponent}
   ];
 
 @NgModule({
